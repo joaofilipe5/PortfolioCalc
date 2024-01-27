560c2363afde56ab659f64bad1011ee1037e1afb
dnferreira13 <danielnferreira@outlook.com>
2024-01-27 00:55:24 +0000

    improved everything

--- calculatorScript_noExcel.py
+++ calculatorScript_noExcel.py
@@ -8,14 +8,14 @@ from datetime import datetime, timedelta
 import quantstats as qs
 
 portfolio= input('Enter the Ticker of the stocks in your portfolio as they appear in Yahoo finance, along with the weights, like this " AAPL:0.5, LSEG.L:0.5" : ')
-portfolio = portfolio.split(',').split(':')
+portfolio = portfolio.split(',')
 for i in portfolio:
-    i=i.upper()
+    i=i.upper().split(':')
 
 class Stock():
-    def __init__(self, symbol, weight, risk_free_rate= yf.Ticker('^TNX').info['regularMarketPrice']/100):
+    def __init__(self, symbol, weight, risk_free_rate):
         self.symbol = symbol
-        self.RF = risk_free_rate
+        self.RF = float(risk_free_rate)
         self.weight= weight
         self.data = None
     
@@ -27,7 +27,7 @@ class Stock():
     def get_data(self, start_date, end_date):
         end_date = datetime.today()
         start_date = end_date - timedelta(days=365)
-        self.data = yf.download(self.symbol, start=start_date, end=end_date, interval='1d')
+        self.data = yf.download(self.symbol, start=start_date, end=end_date, interval='1d', ignore_tz=True)
         return self.data
 
     def getInfo(self):
@@ -72,9 +72,10 @@ class Stock():
         return info  
 
 class Portfolio():
-    def __init__(self, stocks):
+    def __init__(self, stocks, risk_free_rate):
         self.stocks = stocks
         self.data = None
+        self.RF = risk_free_rate
     
     def getExpectedReturn(self):
         expectedReturn = 0
@@ -84,10 +85,24 @@ class Portfolio():
     
     def getVolatility(self):
         returnsMatrix=[]
+        weightsMatrix=[]
         for stock in self.stocks:
             returnsMatrix.append(stock.getReturns())
+            weightsMatrix.append(stock.weight)
         returnsMatrix = pd.concat(returnsMatrix, axis=1)
         returnsMatrix=returnsMatrix.dropna()
         returnsMatrix=np.array(returnsMatrix)
-            
+        weightsMatrix=np.array(weightsMatrix)
+        volatility = np.sqrt(np.dot(weightsMatrix.T, np.dot(np.cov(returnsMatrix), weightsMatrix))) * np.sqrt(252)
+
         return volatility
+    
+    def getPortfolioSharpe(self):
+        sharpe = (self.getExpectedReturn() - self.RF) / self.getVolatility()
+        return sharpe
+    
+
+for i in range(int(len(portfolio))):
+    portfolio[i] = Stock(portfolio[i][0], float(portfolio[i][1]), 0.05)
+    
+portfolio = Portfolio(portfolio, 0.05)
